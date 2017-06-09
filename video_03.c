22d6de7160ac7bb0db6d05b4fb08374228082455
peresrafael1994 <peresrafael1994@gmail.com>
2017-06-09 16:25:21 -0300

    Create video_03.c

---
+++ video_03.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+float a,b;
+
+printf("digite a : ");
+
+scanf("%f",&a);
+
+printf("\ndigite b : ");
+
+scanf("%f",&b);
+
+printf("\n\numa porcentagem de %.2f%%", (a/b)*100);
+
+getchar();
+
+return 0;
+
+}
